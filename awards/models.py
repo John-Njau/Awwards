b2dd6c9362e0a817a3262747af3fb292a16659a7
john.njau <john.njoroge@student.moringaschool.com>
2022-06-12 00:38:17 +0300

    feature: added ratings models

--- awards/models.py
+++ awards/models.py
@@ -1,26 +1,39 @@
-from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import User
+
+from tinymce.models import HTMLField
+from cloudinary.models import CloudinaryField
+
+import datetime as dt
+
 
 class Project(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='media/images/')
-    description = models.TextField()
+    image = CloudinaryField('Awards/project_images')
+    description = HTMLField()
     url = models.URLField(blank=False)
+    # uploaded_at = models.DateTimeField(auto_now_add=True)
+    
     
     def __str__(self):
-        return self.user.username
+        return self.title
+    
+    
+    # class Meta:
+    #     ordering = ['-uploaded_at']
     
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
-    profile_pic = models.ImageField(upload_to='media/profiles/')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_pic = CloudinaryField('Awards/profiles')
     bio = models.CharField(max_length=255)
     # created_at = models.DateTimeField(auto_now_add=True)
     
+    
     def get_user_projects(self):
         return self.projects.all()
+    
     
     def __str__(self):
         return self.user.username
@@ -42,3 +55,44 @@ class UserContacts(models.Model):
     def __str__(self):
         return self.user.username
     
+#  Project review   
+#  RATING=[(
+#      (1, '1'),
+#      (2, '2'),
+#      (3, '3'),
+#      (4, '4'),
+#      (5, '5'),
+#  )]
+    
+class Reviews(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='reviews')
+    review = HTMLField()
+    review_rating = models.CharField( choices=[(1, '1'),(2,'2'), (3 ,'3'), (4,'4'),(5, '5') ],max_length=150)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def get_review_rating(self):
+        return self.review_rating
+        
+    
+    def __str__(self):
+        return self.user.username
+    
+    
+    class Meta:
+        verbose_name_plural = 'Reviews'
+    
+    
+class Ratings(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='ratings')
+    rating = models.IntegerField()
+    # created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username    
+    
+    
+    class Meta:
+        verbose_name_plural = 'Ratings'
