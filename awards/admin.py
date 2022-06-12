be68c28bde9b1bf70c5f99bb2149d493106b73b6
john.njau <john.njoroge@student.moringaschool.com>
2022-06-13 00:41:54 +0300

    feature: registering models in admin

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Project, Profile, UserContacts, Ratings, Reviews
+
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(UserContacts)
+
+admin.site.register(Ratings)
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display= ('user', 'project', 'review', 'review_rating')
+admin.site.register(Reviews, ReviewAdmin)
